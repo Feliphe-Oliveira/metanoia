68eedc382f67fd092f4f83e3c942c1c4964eb6ae
Feliphe-Oliveira <oliveirafeliphe272@gmail.com>
2024-11-07 21:44:59 -0300

    concerto

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 const btn_metanoia = document.getElementById('btnMe')
 btn_metanoia.addEventListener('click',()=>{
-    const cod = 1111
+    const cod = 'cura'
     const senha = document.getElementById('senha')
 
-    if(senha.value == cod){
+    if(senha.value.toUpperCase() == cod.toUpperCase()){
         document.getElementById('cx_verso').classList.add('destaque')
         document.getElementById('login').classList.add('semdestaque')
+    }else if(senha.value == ''){
+        alert('Preencha os dados solicitados')
+    }else{
+        alert('Senha incorreta')
     }
 })
